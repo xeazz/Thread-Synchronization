48a62e5114c5f689e16c2fd0ab643fa45aa898cc
Dmitri Vorobev <vorobiev1401@mail.ru>
2022-11-26 15:00:07 +0300

    correct Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,31 @@ import java.util.Random;
 
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread threadSearchMax = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet()
+                            .stream()
+                            .max(Comparator.comparing(Map.Entry::getValue))
+                            .orElse(null);
+                    System.out.println("Текущий максимум " + maxEntry.getKey() + " (встретилось " + maxEntry.getValue() + " раз)");
+                }
+            }
+        });
+        threadSearchMax.start();
         for (int i = 0; i < 1000; i++) {
-            new Thread(() -> {
+            Thread threadGenerateRoute = new Thread(() -> {
                 int numberOfTurns = 0;
                 String route = generateRoute("RLRFR", 100);
                 System.out.println(route);
-                for (int j = 0; j<route.length(); j++) {
+                for (int j = 0; j < route.length(); j++) {
                     if (route.charAt(j) == 'R') numberOfTurns++;
                 }
                 synchronized (sizeToFreq) {
@@ -20,29 +38,33 @@ public class Main {
                     } else {
                         sizeToFreq.put(numberOfTurns, 1);
                     }
+                    sizeToFreq.notify();
                 }
-            }).start();
+            });
+            threadGenerateRoute.start();
+            threadGenerateRoute.join();
         }
-        printMethod();
+        threadSearchMax.interrupt();
     }
-    public static String generateRoute (String letters, int length) {
+
+    public static String generateRoute(String letters, int length) {
         Random random = new Random();
         StringBuilder route = new StringBuilder();
-        for (int i = 0; i< length; i++) {
+        for (int i = 0; i < length; i++) {
             route.append(letters.charAt(random.nextInt(letters.length())));
         }
         return route.toString();
     }
 
-    public static void printMethod() {
-        Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet()
-                .stream()
-                .max(Comparator.comparing(Map.Entry::getValue))
-                .orElse(null);
-        System.out.println("Самое частое количество повторений " + maxEntry.getKey() + " (встретилось " + maxEntry.getValue() + " раз)");
-        System.out.println("Другие параметры: ");
-        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-            System.out.println("- " + entry.getKey() + " (" +entry.getValue()+ " раз)");
-        }
-    }
+//    public static void printMethod() {
+//        Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet()
+//                .stream()
+//                .max(Comparator.comparing(Map.Entry::getValue))
+//                .orElse(null);
+//        System.out.println("Самое частое количество повторений " + maxEntry.getKey() + " (встретилось " + maxEntry.getValue() + " раз)");
+//        System.out.println("Другие параметры: ");
+//        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+//            System.out.println("- " + entry.getKey() + " (" +entry.getValue()+ " раз)");
+//        }
+//    }
 }
